189eecb027664da752c0804f3889b91c013d6a21
tripleS-dev-design <samiktami.leadform@gmail.com>
2025-07-08 00:24:46 +0200

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -25,7 +25,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "build", "client")));
 
