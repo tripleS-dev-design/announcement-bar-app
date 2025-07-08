2acf0780e33ba84e90279506a2a462fc59ed3ccf
tripleS-dev-design <samiktami.leadform@gmail.com>
2025-07-08 14:11:57 +0200

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -25,7 +25,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
-// 🚀 c'est ça qui sauve tout :
 const port = Number(process.env.PORT) || 8080;
 
 app.use(express.static(path.join(__dirname, "build", "client")));
