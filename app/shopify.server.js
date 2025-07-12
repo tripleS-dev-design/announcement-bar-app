cbd498b57166e7d4c057979173a626ec699e1f4e
tripleS-dev-design <samiktami.leadform@gmail.com>
2025-07-12 02:29:57 +0200

    Update shopify.server.js

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -36,7 +36,7 @@ const shopify = shopifyApp({
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
   apiVersion: ApiVersion.January25,
   scopes: process.env.SCOPES?.split(","),
-  appUrl: process.env.SHOPIFY_APP_URL || "",
+appUrl: process.env.SHOPIFY_APP_URL ?? (() => { throw new Error("❌ SHOPIFY_APP_URL is missing"); })(),
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
