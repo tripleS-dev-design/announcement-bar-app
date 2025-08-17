0e3597d178a0a2c22b9df90e0dd283d640d3009f
tripleS-dev-design <samiktami.leadform@gmail.com>
2025-08-17 21:00:38 +0200

    Delete app/routes/webhooks/shopDataErase.js

--- app/routes/webhooks/shopDataErase.js
+++
@@ -1,6 +0,0 @@
-export async function action({ request }) {
-  const body = await request.text();
-  console.log("🗑️ shop/redact webhook:", body);
-
-  return new Response("Received shop/redact", { status: 200 });
-}
