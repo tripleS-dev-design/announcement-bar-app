4b7de0a1ac823daeb60058b01d0c21979716cc6f
Sami Ktami <ktami.sami@icloud.com>
2025-10-25 05:23:57 +0200

    Revert "merge: résoudre conflits en gardant la version feature (pricing, settings, announcement-premium)"
    This reverts commit 538772ea98d0c47975814ca9eecae8866d38af34, reversing
    changes made to d64d75b6cb2938f55e1416d7ff0722309ba22425.

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,15 +1,9 @@
-// app/routes/_index.jsx
 import { redirect } from "@remix-run/node";
 
 export const loader = async ({ request }) => {
   const url = new URL(request.url);
   const to = new URL("/settings", url.origin);
-
-  // garde tous les paramètres (shop, host, hmac…)
-  for (const [k, v] of url.searchParams.entries()) {
-    to.searchParams.set(k, v);
-  }
-
+  for (const [k, v] of url.searchParams.entries()) to.searchParams.set(k, v);
   return redirect(to.toString());
 };
 
